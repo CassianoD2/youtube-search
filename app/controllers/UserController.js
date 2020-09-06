9eb28118af263929f772b8be7e54afe872f0d01d
Cassiano Mesquita <cassianomesquita@hotmail.com>
2020-09-06 17:32:48 -0300

    Express-Validator installation

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router(); 
 
+const { body, validationResult } = require('express-validator');
 const { Users } = require('../models/index');
 
 router.get('/',(req, res) => {
@@ -12,8 +13,18 @@ router.get('/',(req, res) => {
         );
 });
 
-router.post('/', (req, res) => {
 
+const UsersPostValidation = [
+    body('email').isEmail().withMessage("Utilize um e-mail válido."),
+    body('password').isLength({min: 6}).withMessage("Deve ter no mínimo 6 caracteres."),
+    body('name').exists().withMessage("Preencha o nome.")
+];
+
+router.post('/', UsersPostValidation, (req, res) => {
+    var errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        res.status(400).json({errors: errors.array()});
+    }
 
     res.json(req.body);
 });
