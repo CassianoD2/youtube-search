c76f1c0a72ec5287056c203cdbe7d6d3e0269ccf
Cassiano Erthal de Mesquita <cassianomesquita@hotmail.com>
2021-01-05 22:35:57 -0300

    FEA: Add the search for the video information

--- app/services/YoutubeService.js
+++ app/services/YoutubeService.js
@@ -2,10 +2,12 @@ const axios = require('axios');
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../../config/config.json')[env];
 
+const YOUTUBEAPIKEY = config['youtubeapi'] ? process.env.YOUTUBEAPIKEY : config['youtubeapi'];
+
 var youtubeService = {
     search: (res, text) => {
         var params = new URLSearchParams({
-            key: config['youtubeapi'],
+            key: YOUTUBEAPIKEY,
             part: 'snippet',
             maxResults: 50,
             q: text,
@@ -13,6 +15,29 @@ var youtubeService = {
         });
 
         var videoPush = [];
+
+        var getInfoVideo = async (videoId) => {
+            var params = new URLSearchParams({
+                part: 'snippet, contentDetails',
+                id: videoId,
+                key: YOUTUBEAPIKEY
+            });
+
+            return await axios.get("https://www.googleapis.com/youtube/v3/videos?" + params.toString())
+                .then((result) => {
+                    return {
+                        error: false,
+                        data:result.data
+                    };
+                })
+                .catch((error) => {
+                    return {
+                        error: true,
+                        data: error
+                    }
+                })
+        };
+
         var funcYoutube = async (text, params) => {
             var retorno = await axios.get("https://www.googleapis.com/youtube/v3/search?" + params.toString())
                 .then(
@@ -22,7 +47,8 @@ var youtubeService = {
                                 params.append('pageToken', result.data.nextPageToken);
                             }
 
-                            result.data.items.forEach(element => {
+                            result.data.items.forEach( async element => {
+                                element.videoInfo = await getInfoVideo(element.id.videoId);
                                 videoPush.push(element);
                             });
 
@@ -45,28 +71,6 @@ var youtubeService = {
         }
 
         funcYoutube(text, params);
-    },
-
-    getInfoVideo: async (videoId) => {
-        var params = new URLSearchParams({
-            part: 'snippet, contentDetails',
-            id: videoId,
-            key: process.env.YOUTUBEAPI
-        });
-
-        return await axios.get("https://www.googleapis.com/youtube/v3/videos?" + params.toString())
-            .then((result) => {
-                return {
-                    error: false,
-                    data:result.data
-                };
-            })
-            .catch((error) => {
-                return {
-                    error: true,
-                    data: error
-                }
-            })
     }
 };
 
